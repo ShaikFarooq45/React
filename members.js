a2ce8f1417a34f513ff8518d96546d5fdedc73ea
ShaikFarooq45 <113016888+ShaikFarooq45@users.noreply.github.com>
2024-11-20 00:23:47 +0530

    Jsx Conditional Rendering

---
+++ members.js
@@ -0,0 +1,51 @@
+var members = [
+                {
+                id: 1,
+                name:"Farooq",
+                status: "online",
+                gender : "male",
+
+                },
+
+                {
+                    id: 2,
+                    name:"Sreenu",
+                    status: "online",
+                    gender : "male",
+                    
+                    },
+
+                {
+                    id: 3,
+                    name:"Surekha",
+                    status: "offline",
+                    gender : "female",
+                    
+                    },
+
+
+                    {
+                        id: 4,
+                        name:"geetha",
+                        status: "online",
+                        gender : "female",
+                        
+                        },
+
+                        {
+                            id: 5,
+                            name:"seetha",
+                            status: "online",
+                            gender : "female",
+                            
+                            },
+
+                            {
+                                id: 6,
+                                name:"Rishi",
+                                status: "offline",
+                                gender : "male",
+                                
+                                }
+
+              ]
